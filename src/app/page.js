40b624b6b4a95e3b2aabb0e80b02718b33ffd983
Tiran Jayasekara <70306643+Tiran2385@users.noreply.github.com>
2024-01-24 13:54:59 +0530

    update
    update

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 export default function Home() {
   return (
     <>
+      {/* Pushing The Envelop of Software Development */}
       <div className="flex md:flex-row flex-col md:mt-14 w-full">
         <div className="md:w-1/2 w-full">
           <img src="assests/pushing.png" className="w-full h-auto" alt="Empite Logo" />
@@ -83,19 +84,19 @@ export default function Home() {
 
       </div>
 
-      {/* Capabilities*/}
+      {/* Capabilities / ShowCase */}
       <div className="flex md:flex-row flex-col w-full">
 
         <div className="md:w-1/2 flex flex-col items-start relative">
           <img src="assests/meeting.png" className="w-auto h-auto" alt="Bracket" />
-          <div className="absolute xl:-mt-64 xl:-ml-0 lg:-mt-40 lg:-ml-4 md:-mt-32 md:-ml-4 -mt-20 -ml-0 w-full h-full flex flex-col justify-end text-center md:text-left">
-            <p className="text-gray-300 text-sm font-muli md:text-sm lg:ml-28 md:ml-14 lg:-mt-32 md:-mt-10">CAPABILITIES</p>
-            <p className="font-playfair-semibold md:text-xl lg:text-4xl xl:text-5xl text-xl lg:ml-28 md:ml-14 mt-6 text-white">
+          <div className="absolute md:top-28 top-14 left-0 w-full ">
+            <p className="text-gray-300 text-sm font-muli md:text-sm md:ml-28 xl:mt-10 md:text-left text-center">CAPABILITIES</p>
+            <p className="font-playfair-semibold md:text-3xl lg:text-4xl xl:text-5xl text-xl lg:ml-28 md:ml-28 mt-6 md:text-left text-center text-white">
               Mobile.<br></br> Web.<br></br> Cloud.
             </p>
-            <button className="w-48 text-sm items-center mx-auto justify-center md:mx-0 md:justify-start mt-8 py-2 px-5 lg:ml-28 md:ml-14 flex flex-row text-white rounded bg-black">
+            <button className="w-48 text-sm items-center mx-auto justify-center md:mx-0 md:justify-start mt-8 py-2 px-5 lg:ml-28 md:ml-28 flex flex-row text-white rounded bg-black">
               View Services
-              <img src="assests/next.png" className="w-auto h-auto ml-4" alt="Next" />
+              <img src="assests/next.png" className="w-6 h-4 ml-4" alt="Next" />
             </button>
           </div>
         </div>
@@ -103,19 +104,97 @@ export default function Home() {
 
         <div className="md:w-1/2 flex flex-col items-start relative">
           <img src="assests/meeting2.png" className="w-auto h-auto" alt="Bracket" />
-          <div className="absolute xl:-mt-64 xl:-ml-0 lg:-mt-40 lg:-ml-4 md:-mt-32 md:-ml-4 -mt-20 -ml-0 w-full h-full flex flex-col justify-end text-center md:text-left">
-            <p className="text-gray-300 text-sm font-muli md:text-sm lg:ml-28 md:ml-14 lg:-mt-32 md:-mt-10">SHOWCASE</p>
-            <p className="font-playfair-semibold md:text-xl lg:text-4xl xl:text-5xl text-xl lg:ml-28 md:ml-14 mt-6 text-white">
+          <div className="absolute md:top-28 top-14 left-0 w-full ">
+            <p className="text-gray-300 text-sm font-muli md:text-sm md:ml-28 xl:mt-10 md:text-left text-center">SHOWCASE</p>
+            <p className="font-playfair-semibold md:text-3xl lg:text-4xl xl:text-5xl text-xl lg:ml-28 md:ml-28 mt-6 md:text-left text-center text-white">
               Our most recent<br></br> projects.<br></br><br></br>
             </p>
-            <button className="w-48 text-sm items-center mx-auto justify-center md:mx-0 md:justify-start mt-8 py-2 px-5 lg:ml-28 md:ml-14 flex flex-row text-white rounded bg-black">
+            <button className="w-48 text-sm items-center mx-auto justify-center md:mx-0 md:justify-start mt-8 py-2 px-5 lg:ml-28 md:ml-28 flex flex-row text-white rounded bg-black">
               View Projects
-              <img src="assests/next.png" className="w-auto h-auto ml-4" alt="Next" />
+              <img src="assests/next.png" className="w-6 h-4 ml-4" alt="Next" />
             </button>
           </div>
         </div>
 
       </div>
+
+      {/* We help to create Digital strategies. */}
+      <div className="flex md:flex-row flex-col w-full mt-20 mb-20">
+        <div className="md:w-1/2 w-full md:ml-4">
+          <p className="text-gray-900 text-sm font-muli lg:ml-24 md:ml-20 md:text-left text-center">CAPABILITIES</p>
+          <p className="font-playfair-semibold md:text-4xl lg:text-5xl xl:text-6xl text-3xl lg:ml-24 md:ml-20 mt-4 md:mt-10  md:text-left text-center text-black">
+            We help to create <br></br> Digital strategies.
+          </p>
+          <p className="font-space-grotesk font-bold mt-8 text-base text-black lg:ml-24 md:ml-20 md:text-left text-center">Website & Mobile Development</p>
+          <div className="relative w-9/12 h-2 bg-gray-300 lg:ml-24 md:ml-20 rounded-2xl md:mt-10 lg:mt-1 mx-auto mt-10">
+            <div className="absolute h-full bg-red-600 w-9/12 rounded-2xl"></div>
+
+            {/* Percentage Label */}
+            <div className="absolute h-full w-10/12 flex items-center justify-end pr-2">
+              <div className="flex bg-black rounded-sm -mt-14">
+                <p className="text-white text-xs p-1 font-bold">86%</p>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+
+        <div className="md:w-1/2 w-full text-center md:text-left">
+          <p className="font-space-grotesk font-bold mt-8 text-base text-black md:ml-24">- Website & Mobile App Design</p>
+          <p className="font-muli text-xs md:ml-24 mt-6 lg:mr-56 md:mr-20 md:text-left mx-10 md:mx-0">We’ve created a full-stack structure for our working <b>workflow processes</b>, were from the funny the century initial all the made, have spare to negatives.</p>
+          <p className="font-space-grotesk font-bold mt-8 text-base text-black md:ml-24">+ Motion Graphics & Animation</p>
+          <p className="font-space-grotesk font-bold mt-8 text-base text-black md:ml-24">+ User Experience</p>
+        </div>
+
+      </div>
+
+      {/* Gallery */}
+      <div className="flex md:flex-row flex-col w-full mb-10">
+        <div className="md:w-7/12 relative">
+          <img src="assests/collection/Mask4.png" className="w-full h-5/6 object-cover" alt="Mask4" />
+          <div className="absolute top-0 md:-mt-12 md:ml-10 left-0 w-full h-full flex items-center justify-center">
+            {/* icon goes here */}
+            <img src="assests/collection/playButton.png" className="w-12 h-12 object-cover" alt="Mask4" />
+          </div>
+
+        </div>
+
+
+        <div className="md:w-5/12 flex flex-row ">
+          <div className="md:w-6/12 ">
+            <img src="assests/collection/Mask5.png" className="w-full md:h-5/6 h-3/5" alt="Mask5" />
+          </div>
+          <div className="md:w-4/12 ">
+            <img src="assests/collection/Mask6.png" className="w-full md:h-5/6 h-3/5" alt="Mask6" />
+          </div>
+          <div className="md:w-2/12">
+
+          </div>
+
+        </div>
+      </div>
+
+      {/* Succeeded project */}
+      <div className="flex flex-row w-full">
+        <div className="w-7/12">
+          <div className="flex flex-row">
+            <div className="w-1/2 flex flex-col">
+            <img src="assests/collection/playButton.png" className="w-auto h-auto object-cover" alt="Mask4" />
+
+            </div>
+            <div className="w-1/2 flex flex-col">
+            <img src="assests/collection/playButton.png" className="w-auto h-auto object-cover" alt="Mask4" />
+              
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+
+
     </>
   );
 }
